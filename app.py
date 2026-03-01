2c549a2e94cf44745deaea903902e1646ee09863
Umesh-Bhatt-0507 <Ashubhatt987833@gmail.com>
2026-03-01 20:21:49 +0530

    Added route for add

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from sqlalchemy import URL
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -23,6 +23,25 @@ class Note(db.Model):
     title=db.Column(db.String(200))
     content=db.Column(db.Text)
     created_at=db.Column(db.DateTime,default=datetime.utcnow)
+
+@app.route('/')
+def home():
+    notes=Note.query.all()
+    return render_template("home.html",notes=notes)
+
+@app.route('/add',methods=["GET","POST"])
+def add():
+    if request.method=="POST":
+        title=request.form["title"]
+        content=request.form["content"]
+        note=Note(
+            title=title,
+            content=content,
+        )
+        db.session.add(note)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_note.html")
 
 
 if __name__ == '__main__':
